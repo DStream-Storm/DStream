e1d5f78e1dfc261a464969f643c6e8004ffe911a
MasterTj <798750509@qq.com>
2019-03-25 20:41:56 +0800

    update Constraints...

--- src/main/java/com/basic/core/Constraints.java
+++ src/main/java/com/basic/core/Constraints.java
@@ -8,11 +8,12 @@ import com.basic.core.util.PropertiesUtil;
  * Created by tj on 2017/5/12.
  */
 public class Constraints {
+
     public static int Threshold_r = Integer.valueOf(PropertiesUtil.getProperties("Threshold_r"));//
     public static int Threshold_l = Integer.valueOf(PropertiesUtil.getProperties("Threshold_l"));//
     public static double Threshold_p = Double.valueOf(PropertiesUtil.getProperties("Threshold_p"));//Attenuation probability
 
-    public static final String SPLITTER_BOLT_ID = "splitter-bolt";
+    public static final String SCHEDULER_BOLT_ID = "scheduler-bolt";
     public static final String COIN_BOLT_ID= "coin-bolt";
     public static final String PREDICTOR_BOLT_ID= "predictor-bolt";
 
